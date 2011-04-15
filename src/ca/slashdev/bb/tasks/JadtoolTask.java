89b17ca58cba9dba0b22153cc0d6f61a3bdc20cf
Josh Kropf <jiggak@users.sourceforge.net>
2011-04-15 16:14:54 -0400

    renamed add(OverrideType) to addOverride(OverrideType) based on user feedback suggesting the add() method doesn't work

--- src/ca/slashdev/bb/tasks/JadtoolTask.java
+++ src/ca/slashdev/bb/tasks/JadtoolTask.java
@@ -1,18 +1,18 @@
 /*
  * Copyright 2008 Josh Kropf
- * 
+ *
  * This file is part of bb-ant-tools.
- * 
+ *
  * bb-ant-tools is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * bb-ant-tools is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with bb-ant-tools; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -49,7 +49,7 @@ public class JadtoolTask extends BaseTask {
    private Vector<ResourceCollection> resources = new Vector<ResourceCollection>();
    private Vector<OverrideType> overrides = new Vector<OverrideType>();
    private Map<String, OverrideType> overrideMap = new HashMap<String, OverrideType>();
-   
+
    /**
     * Input .jad file.
     * @param input
@@ -57,7 +57,7 @@ public class JadtoolTask extends BaseTask {
    public void setInput(File input) {
       this.input = input;
    }
-   
+
    /**
     * Destination directory for cod files and new jad file.
     * @param destDir
@@ -65,7 +65,7 @@ public class JadtoolTask extends BaseTask {
    public void setDestDir(File destDir) {
       this.destDir = destDir;
    }
-   
+
    /**
     * Set output .jad file name. Use name of input file by default.
     * @param output
@@ -73,106 +73,106 @@ public class JadtoolTask extends BaseTask {
    public void setOutput(String output) {
       this.output = output;
    }
-   
+
    public void add(ResourceCollection res) {
       resources.add(res);
    }
-   
-   public void add(OverrideType override) {
+
+   public void addOverride(OverrideType override) {
       overrides.add(override);
    }
-   
+
    @Override
    public void execute() throws BuildException {
       super.execute();
-      
+
       if (input == null)
          throw new BuildException("input is a required attribute");
-      
+
       if (!input.exists())
          throw new BuildException("input file is missing");
-      
+
       if (output == null)
     	  output = input.getName();
-      
+
       if (resources.size() == 0)
          throw new BuildException("specify at least one cod file");
-      
+
       if (destDir == null)
          throw new BuildException("destdir is a required attribute");
-      
+
       if (!destDir.exists())
          if (!destDir.mkdirs())
             throw new BuildException("unable to create destination directory");
-      
+
       for (OverrideType o : overrides) {
          o.validate();
          overrideMap.put(o.getKey().toLowerCase(), o);
       }
-      
+
       executeRewrite();
    }
-   
+
    @SuppressWarnings("unchecked")
    private void executeRewrite() {
       BufferedReader reader = null;
       PrintStream output = null;
-      
+
       try {
          try {
             reader = new BufferedReader(new FileReader(input));
             output = new PrintStream(new File(destDir, this.output));
-            
+
             int i, num = 0;
             String line, key, value;
             OverrideType override;
-            
+
             while ((line = reader.readLine()) != null) {
                num ++;
-               
+
                i = line.indexOf(':');
                if (i == -1)
                   throw new BuildException("unexpected line in jad file: "+num);
-               
+
                key = line.substring(0, i);
                value = line.substring(i+1).trim();
-               
+
                if (key.startsWith("RIM-COD-URL")
                      || key.startsWith("RIM-COD-SHA1")
                      || key.startsWith("RIM-COD-Size")) {
                   continue; // ignore line
                }
-               
+
                // check for .jad element override, remove from map if found
                override = overrideMap.get(key.toLowerCase());
                if (override != null) {
                   value = override.getValue();
                   overrideMap.remove(key.toLowerCase());
                }
-               
+
                output.printf("%s: %s\n", key, value);
             }
          } catch (IOException e) {
             throw new BuildException("error creating jad file", e);
          }
-         
+
          try {
             int num = 0;
             File destFile;
             Resource r;
-            
+
             for (ResourceCollection rc : resources) {
                Iterator<Resource> i = rc.iterator();
                while (i.hasNext()) {
                   r = i.next();
                   destFile = new File(destDir, Utils.getFilePart(r));
-                  
+
                   if (Utils.isZip(r)) {
                      String[] zipEntries = Utils.extract(r, destDir);
-                     
+
                      for (String entry : zipEntries) {
                         destFile = new File(destDir, entry);
-                        
+
                         if (num == 0) {
                            output.printf("RIM-COD-URL: %s\n", destFile.getName());
                            output.printf("RIM-COD-SHA1: %s\n", Utils.getSHA1(destFile));
@@ -182,12 +182,12 @@ public class JadtoolTask extends BaseTask {
                            output.printf("RIM-COD-SHA1-%d: %s\n", num, Utils.getSHA1(destFile));
                            output.printf("RIM-COD-Size-%d: %d\n", num, destFile.length());
                         }
-                        
+
                         num++;
                      }
                   } else {
                      ResourceUtils.copyResource(r, new FileResource(destFile));
-                     
+
                      if (num == 0) {
                         output.printf("RIM-COD-URL: %s\n", destFile.getName());
                         output.printf("RIM-COD-SHA1: %s\n", Utils.getSHA1(destFile));
@@ -199,13 +199,13 @@ public class JadtoolTask extends BaseTask {
                      }
                      num ++;
                   }
-                  
+
                }
             }
          } catch (IOException e) {
             throw new BuildException("error copying cod file", e);
          }
-         
+
          // flush remaining overrides into target .jad
          for (OverrideType override : overrideMap.values()) {
              output.printf("%s: %s\n", override.getKey(), override.getValue());
